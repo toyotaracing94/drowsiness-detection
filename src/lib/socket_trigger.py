b9c0c5085432b8a27889f1d00cc2dca43e17bbec
Muhammad Juniarto <cc.magang94@toyota.co.id>
2025-05-04 23:36:12 +0700

    change the draw for visualizations and add Socket Trigger for events

---
+++ src/lib/socket_trigger.py
@@ -0,0 +1,89 @@
+
+import json
+import numpy as np
+import cv2
+import base64
+import datetime
+
+from websockets.sync.client import connect
+
+from src.utils.logging import logging_default
+
+class SocketTrigger:
+    def __init__(self, api_settings_path : str):
+        
+        # Load configurations first
+        self.load_configurations(api_settings_path)
+
+
+    def load_configurations(self, path : str) -> None:
+        """
+        Load the detection settings from a configuration JSON file.
+
+        Parameters
+        ----------
+        path : str
+            Path to the configuration file containing api settings to save the triggered event image.
+        """ 
+        logging_default.info("Loading api configs and model configuration")
+        
+        with open(path, 'r') as f:
+            config = json.load(f)
+        
+        self.vehicle_id = config["vehicle_id"]
+        self.server_ip = config["server"]
+        self.device_name = config["device"]
+        
+        # Construct the WebSocket URL
+        self.ws_url = f"ws://{self.server_ip}?vehicle_id={self.vehicle_id}&device={self.device_name}"
+
+    def save_image(self, image : np.ndarray, event, target : str = '', wsEvent : str = '') -> None:
+        """
+        Save the image locally, encode it in base64, and send it to the WebSocket server.
+
+        Parameters
+        ----------
+        image : np.ndarray
+            The image to be saved and sent.
+        
+        event : str
+            The event related to the image (e.g., "drowsiness_detected").
+        
+        target : str, optional
+            The target associated with the event (default is '').
+        
+        wsEvent : str, optional
+            The WebSocket event type (default is '').
+
+        Raises
+        ------
+        Exception
+            If there is an error during the process (e.g., saving, encoding, or sending).
+        """
+        try:
+            # Log the beginning of the process
+            logging_default.info(f"Saving image for event: {event}, target: {target}, websocket event: {wsEvent}")
+
+            with connect(self.ws_url) as websocket:
+                # Save the file in the local system
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                cv2.imwrite(f"image_event/frame{timestamp}.jpg", image)
+
+                with open(f"image_event/frame{timestamp}.jpg", "rb") as image_file:
+                    encoded_string = base64.b64encode(image_file.read())
+                    jsonData = {
+                        "event" : wsEvent,
+                        "vehicle_id" : self.vehicle_id,
+                        "target" : target,
+                        "data" : {
+                            "message": "Image Upload",
+                            "image": "%s"%encoded_string,
+                            "behavior_type" : event
+                        }
+                    }
+                    websocket.send(json.dumps(jsonData))
+                    message = websocket.recv()
+                    logging_default.info(f"Image saved successfully for event: {event}. Message : {message}")
+
+        except Exception as e:
+            logging_default.error(f"Error saving image for event: {event}. Error: {e}")
--- src/services/drowsiness_detection_service.py
+++ src/services/drowsiness_detection_service.py
@@ -2,12 +2,19 @@ import cv2
 from src.hardware.camera import Camera
 from src.lib.drowsiness_detection import DrowsinessDetection
 from src.lib.pose_detection import PoseDetection
+from src.lib.socket_trigger import SocketTrigger
 
+# Initailize the hardware and the lib services
 camera = Camera()
+socket_trigger = SocketTrigger("config/api_settings.json")
 drowsiness_detector = DrowsinessDetection("config/drowsiness_detection_settings.json")
 pose_detector = PoseDetection("config/pose_detection_settings.json")
 
 def generate_original_capture_stream():
+    """
+    This function special for the FastAPI backend controller to continuously captures frames from the camera
+    and return a stream for real-time display transmission
+    """
     while True:
         # Capture the video stream
         ret, frame = camera.get_capture()
@@ -25,10 +32,28 @@ def generate_original_capture_stream():
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 def generate_drowsiness_stream():
+    """
+    This function special for the FastAPI backend controller to continuously captures frames from the camera, processes them for drowsiness detection, 
+    and generates back a video stream with annotated landmarks and detection results.
+
+    This function performs the following operations on each frame:
+    1. Captures a video frame from the camera.
+    2. Detects multi-face landmarks using a drowsiness detection model.
+    3. Detects hand landmarks and body pose.
+    4. Identifies if the user is making a phone call based on body pose.
+    5. Estimates head pose (yaw, pitch, roll) and draws corresponding annotations.
+    6. Checks for drowsiness and yawning based on eye aspect ratio (EAR) and mouth aspect ratio (MAR).
+    7. Draws annotations for detected landmarks (eyes, mouth, hands).
+    8. Encodes the processed frame as a JPEG image for streaming.
+
+    The resulting frames are yielded as a stream for real-time display or transmission.
+    """
     while True:
         # Capture the video stream
         ret, frame = camera.get_capture()
         frame = cv2.flip(frame, 1) 
+
+        # Copy the frame to be passed around and keep the original one
         image = frame.copy()
         if not ret:
             break
@@ -36,11 +61,11 @@ def generate_drowsiness_stream():
         # Get the Multi-Face Mesh Landmarks (486 points)
         face_landmarks = drowsiness_detector.detect_landmarks(frame)
         # Find hand landmarks
-        hand_results = pose_detector.detect_hand_landmarks(image)
+        hand_results = pose_detector.detect_hand_landmarks(frame)
         # Get the body-pose
         body_pose = pose_detector.detect_body_pose(frame)
 
-        # Phone usage from pose
+        # Phone usage feature get from pose information
         is_calling, dist = pose_detector.detect_phone_usage(
             body_pose,
             frame_width=frame.shape[1],
@@ -53,7 +78,7 @@ def generate_drowsiness_stream():
         if dist is not None:
             cv2.putText(image, f"Distance: {dist:.2f}", (20, frame.shape[0] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
 
-
+        # Detect the drowsiness and the head pose estimation from the face landmarks information
         if face_landmarks.multi_face_landmarks:
             for face_landmarks in face_landmarks.multi_face_landmarks:
                 # Get the head pose (yaw, pitch, roll)
@@ -101,17 +126,59 @@ def generate_drowsiness_stream():
                 if drowsiness_detector.check_yawning(mar):
                     cv2.putText(image, "Yawning!", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
-                # Draw the result of the detection
+                # Draw the result of the eye drowsiness detection
                 if left_eye_landmark and right_eye_landmark:
-                    for pt in left_eye_landmark:
-                        cv2.circle(image, pt, 1, (0, 255, 0), -1)
-                    for pt in right_eye_landmark:
-                        cv2.circle(image, pt, 1, (0, 255, 0), -1)
+                    # Draw connected circles for left eye
+                    for i in range(len(left_eye_landmark) - 1):
+                        pt1 = left_eye_landmark[i]
+                        pt2 = left_eye_landmark[i + 1]
+                        
+                        # Draw a line between consecutive points in green
+                        cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                        # Draw circles at the points in green
+                        cv2.circle(image, pt1, 2, (0, 255, 0), -1)
+                    
+                    # To close the left eye loop, connect the last point to the first in green
+                    pt1 = left_eye_landmark[-1]
+                    pt2 = left_eye_landmark[0]
+                    cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                    cv2.circle(image, pt1, 2, (0, 255, 0), -1)
+
+                    # Draw connected circles for right eye
+                    for i in range(len(right_eye_landmark) - 1):
+                        pt1 = right_eye_landmark[i]
+                        pt2 = right_eye_landmark[i + 1]
+                        
+                        # Draw a line between consecutive points in green
+                        cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                        # Draw circles at the points in green
+                        cv2.circle(image, pt1, 2, (0, 255, 0), -1)
+                    
+                    # To close the left eye loop, connect the last point to the first in green
+                    pt1 = right_eye_landmark[-1]
+                    pt2 = right_eye_landmark[0]
+                    cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                    cv2.circle(image, pt1, 2, (0, 255, 0), -1)
 
                 # Draw the mouth
                 if mouth_eye_landmark:
-                    for pt in mouth_eye_landmark:
-                        cv2.circle(image, pt, 1, (255, 0, 0), -1)
+                    # Drawing the line between the middle top and bottom of the lips
+                    cv2.line(image, mouth_eye_landmark[2], mouth_eye_landmark[6], (0, 255, 255), 2)
+
+                    for i in range(len(mouth_eye_landmark) - 1):
+                        pt1 = mouth_eye_landmark[i]
+                        pt2 = mouth_eye_landmark[i + 1]
+                        
+                        # Draw a line between consecutive points
+                        cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                        # Draw circles at the points
+                        cv2.circle(image, pt1, 2, (0, 0, 255), -1)
+
+                    pt1 = mouth_eye_landmark[-1]
+                    pt2 = mouth_eye_landmark[0]
+                    cv2.line(image, pt1, pt2, (0, 255, 0), 1)
+                    cv2.circle(image, pt1, 2, (0, 0, 255), 1)
+
 
         # Draw hand landmarks
         if hand_results.multi_hand_landmarks:
