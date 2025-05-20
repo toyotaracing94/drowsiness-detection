d758216a3a0fe707387130b4be6109cd7b00e84c
Muhammad Juniarto <cc.magang94@toyota.co.id>
2025-05-20 11:13:15 +0700

    change python version from `3.10.15' to just `3.10`

--- scripts/setup_env_raspberry.sh
+++ scripts/setup_env_raspberry.sh
@@ -5,7 +5,7 @@
 # ===============================
 
 VENV_DIR="venv"
-PYTHON_VERSION="3.10.15"
+PYTHON_VERSION="3.10"
 REQUIREMENTS_FILE="requirements-linux.txt"
 
 echo "=========================================="
