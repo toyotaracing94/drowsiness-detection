5b1156946212be4ac885624623d04ca44c29d932
Muhammad Juniarto <cc.magang94@toyota.co.id>
2025-05-21 10:47:03 +0700

    change the camera class name for the raspberry pi system

--- src/hardware/factory_hardware.py
+++ src/hardware/factory_hardware.py
@@ -3,12 +3,8 @@ import os
 
 def get_camera():
     if os.name == "posix":
-        try:
-            from src.hardware.camera.rpi_camera import PiCamera
-            return PiCamera()
-        except ImportError:
-            from src.hardware.camera.cv_camera import CVCamera
-            return CVCamera()
+        from src.hardware.camera.rpi_camera import RPiCamera
+        return RPiCamera()
     else:
         from src.hardware.camera.cv_camera import CVCamera
         return CVCamera()
